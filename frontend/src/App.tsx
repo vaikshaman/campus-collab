845f2bf03663bbd1644279133056d823d1b4a777
utkarsh.ver <utkarshv2005@gmail.com>
2024-01-27 19:58:39 +0530

    auth

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+// app.js
+import React, { useState } from 'react';
+import './App.css';
+import { useMsal } from '@azure/msal-react';
+
+function App() {
+  const { instance, accounts } = useMsal();
+  const [m_strUser, setm_strUser] = useState<string>("");
+
+  const handleLogin = async () => {
+    try {
+      // Check if the user is already logged in
+      if (accounts.length === 0) {
+        // If not logged in, initiate the login process
+        const loginResponse = await instance.loginPopup();
+        const username = loginResponse.account?.username;
+        setm_strUser(username || "");
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+    }
+  };
+
+  const handleLogout = () => {
+    instance.logout();
+    setm_strUser("");
+  };
+
+  return (
+    <div className="App">
+      <h1>Welcome to Your App</h1>
+      {m_strUser !== "" ? (
+        <div>
+          <p>User: {m_strUser}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <p>This is some content on your homepage.</p>
+          <button onClick={handleLogin}>Login with Microsoft</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
