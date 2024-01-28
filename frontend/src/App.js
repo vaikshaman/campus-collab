2c336a5e6680166b3819790f8f9a964834b72467
utkarsh.ver <utkarshv2005@gmail.com>
2024-01-29 02:12:43 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,96 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Navbar from './components/header/Navbar'
-import { useMsal } from '@azure/msal-react';
+import Navbar from './components/header/Navbar';
 import Sidebar from './components/pages/Profile/Sidebar';
+import AzureAuth from '../src/auth/azureRegisteration';
 
 function App() {
-  const { instance, accounts } = useMsal();
-  const [m_strUser, setm_strUser] = useState("");
-
-  
-  // const handleLogin = async () => {
-  //   try {
-  //     // Check if the user is already logged in
-  //     if (accounts.length === 0) {
-  //       // If not logged in, initiate the login process
-  //       const loginResponse = await instance.loginPopup();
-  //       const username = loginResponse.account?.username;
-  //       console.log(loginResponse);
-  //       console.log(username);
-  //       setm_strUser(username || "");
-  //     }
-  //   } catch (error) {
-  //     console.error('Login error:', error);
-  //   }
-  // };
-  const handleLogin = async () => {
-    try {
-      if (accounts.length === 0) {
-        const loginResponse = await instance.loginPopup();
-        const username = loginResponse.account?.username;
-        
-        setm_strUser(username || "");
-  
-        
-        await postDataToBackend({ m_strUser });
-      }
-    } catch (error) {
-      console.error('Login error:', error);
-    }
-  };
-  
-  const postDataToBackend = async (data) => {
-    try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to post login data to backend');
-      }
-  
-      console.log('Login data posted to backend successfully');
-    } catch (error) {
-      console.error('Error posting login data to backend:', error);
-    }
-  };
-  
-
-
-
-  const handleLogout = () => {
-
-    instance.logout();
-    
-    setm_strUser("");
-  };
-
   return (
-    
     <div className="App">
-        <Navbar/>
-        <Sidebar />
-    <h1>Welcome to Your App</h1>
-    {m_strUser !== "" ? (
-      <div>
-        <p>User: {m_strUser}</p>
-        <button onClick={handleLogout}>Logout</button>
-      </div>
-    ) : (
-      <div>
-        <p>This is some content on your homepage.</p>
-        <button onClick={handleLogin}>Login with Microsoft</button>
-      </div>
-
-    )}
-  </div>
-  
-    
+      <Navbar />
+      <Sidebar />
+      {/*<AzureAuth />*/}
+    </div>
   );
 }
 
---
+++ frontend/src/auth/azureRegisteration.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useMsal } from '@azure/msal-react';
+
+function AzureAuth() {
+  const { instance, accounts } = useMsal();
+  const [m_strUser, setm_strUser] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      // Check if the user is already logged in
+      if (accounts.length === 0) {
+        // If not logged in, initiate the login process
+        const loginResponse = await instance.loginPopup();
+        const username = loginResponse.account?.username;
+        setm_strUser(username || "");
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+    }
+  };
+
+  const handleLogout = () => {
+    instance.logout();
+    setm_strUser("");
+  };
+
+  return (
+    <div className="auth">
+      <h1>Welcome to Your App</h1>
+      {m_strUser !== "" ? (
+        <div>
+          <p>User: {m_strUser}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <p>This is some content on your homepage.</p>
+          <button onClick={handleLogin}>Login with Microsoft</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AzureAuth;
