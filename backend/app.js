883580789ee8711229d1f634f68914460a8d1a03
vibhanshu <vibhanshuiit@gmail.com>
2024-02-01 01:53:10 +0530

    notify

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 // Import statements
 import express from "express";
 import dotenv from 'dotenv';
-import connectDB from './config/db.js'; // Import from the correct file path
+import connectDB from './config/db.js';
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import router from './routes/router.js'; // Import from the correct file path
+import router from './routes/router.js';
+import notifictionRouter from './routes/notificationRouter.js'
 
 // Configure dotenv
 dotenv.config();
@@ -16,22 +17,23 @@ connectDB();
 const app = express();
 
 // Set view engine
-app.set("view engine","es");
+app.set("view engine", "es");
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 // Middleware for parsing incoming request bodies
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Use router middleware
 app.use(router);
+app.use('/api/notification', notifictionRouter)
 
 // Define the port
 const PORT = process.env.PORT || 4000;
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server Started on port ${PORT}`);
+    console.log(`Server Started on port ${PORT}`);
 });
--- backend/models/profileModel.js
+++ backend/models/profileModel.js
@@ -1,19 +1,16 @@
 import mongoose from "mongoose";
 
 
-const ProfileSchema = mongoose.Schema(
-    {
-        photo: { type: String }, 
-        name: { type: String, required: true },
-        email: { type: String, required: true },
-        age: { type: Number, required: true },
-        institute: { type: String, required: true },
-        branch: { type: String, required: true },
-    },
-    {
-        timestamps: true
-    }
-);
+const ProfileSchema = mongoose.Schema({
+    photo: { type: String },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: true },
+    institute: { type: String, required: true },
+    branch: { type: String, required: true },
+}, {
+    timestamps: true
+});
 
 
 export default mongoose.model("Profile", ProfileSchema);
