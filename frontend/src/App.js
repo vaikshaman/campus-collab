cd7544b91698802eb419af87471416d9ad5f62af
ShashwatSharma15 <shashwatsharma.mohali@gmail.com>
2024-01-31 22:17:17 +0530

    merger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import Navbar from './components/header/Navbar';
 import Sidebar from './components/pages/Profile/Sidebar';
 import AzureAuth from '../src/auth/azureRegisteration';
 import Profile from './components/pages/Profile/Profile';
+import Home from './components/pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Profile/>
+      {/* <Profile/> */}
       {/* <AzureAuth /> */}
-      {/* <Home/> */}
+      <Home/>
       {/* <Footer/> */}
     </div>
   );
