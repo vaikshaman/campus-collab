ad1db9ed050a7dce6589c41092820b289e5f48cd
ShashwatSharma15 <shashwatsharma.mohali@gmail.com>
2024-02-02 03:52:21 +0530

    working on project section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,17 @@ import Profile from './components/pages/Profile/Profile';
 import AppRoutes from './AppRoutes';
 import EditProfile from './components/pages/Profile/EditProfile';
 import Home from './components/pages/Home/Home';
+import Project from './components/pages/Project/Project';
 
 function App() {
   return (
     <div className="App">
       {/* <Profile/> */}
       {/* <AzureAuth /> */}
-      <Home/>
+      {/* <Home/> */}
       {/* <Footer/> */}
-      <EditProfile />
+      {/* <EditProfile /> */}
+      <Project/>
     </div>
   );
 }
