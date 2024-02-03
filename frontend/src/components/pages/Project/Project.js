aefe95ac5f2cde32f3c65eaa1a34ddad5407443f
utkarsh.ver <utkarshv2005@gmail.com>
2024-02-04 02:51:16 +0530

    project

--- frontend/src/components/pages/Project/Project.js
+++ frontend/src/components/pages/Project/Project.js
@@ -1,33 +1,69 @@
-// import { useState } from "react";
-// import React from "react";
+import React, { useState, useEffect } from 'react';
+import Navbar from '../../header/Navbar';
+import "./Project.css"
 
-// const Project=()=>{
-// return(
-//     <div className="container">
-//         <div className="Project-desc">
-//         </div>
-//             <div className="project-img">
-//                 <img src="" alt="" />
-//             </div>
-//             <div className="Project-code">
 
-//             </div>
-//             <div className="Project-post">
-//             <div className="Project-completed">
+const Project = () => {
+  const [inputFields, setInputFields] = useState([]);
 
-//             </div>
-//             <div className="Project-reviews">
-//                 <input type="text" />
-//                 <div className="Project-comments">
+  useEffect(() => {
+    // Retrieve stored input fields from localStorage on component mount
+    const storedInputFields = JSON.parse(localStorage.getItem('inputFields'));
+    if (storedInputFields) {
+      setInputFields(storedInputFields);
+    }
+  }, []);
 
-//                 <div className='confirm-content'>
-//                     <button className='remove-btn'>Discard</button>
-//                     <button className='continue-btn'>Continue</button>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//   );
-// };
+  const addInputField = (type) => {
+    const newInputField = { type };
+    setInputFields((prevFields) => [...prevFields, newInputField]);
+  };
+
+  useEffect(() => {
+    // Store input fields to localStorage whenever inputFields state changes
+    localStorage.setItem('inputFields', JSON.stringify(inputFields));
+  }, [inputFields]);
+
+  return (
+    <div>
+        
+        
+        <div className='project-main'>
+        <Navbar/>
+            <div className='content-shown'>
+                <form>
+                    {inputFields.map((field, index) => (
+                    <div key={index}>
+                        <input type="text" className={`project-${field.type}`} placeholder={`Enter ${field.type} here`} />
+                    </div>
+                    ))}
+                </form>
+            </div>
+
+            <div className='add-box'>
+                <div className='add-content'>
+                    Add Content
+                    <div className='add-btns'>
+                        <button className='main-btn' onClick={() => addInputField('heading')}>Add Heading</button>
+                        <button className='main-btn' onClick={() => addInputField('subheading')}>Add Subheading</button>
+                        <button className='main-btn' onClick={() => addInputField('description')}>Add Description</button>
+                        <button className='main-btn' onClick={() => addInputField('caption')}>Add Caption</button>
+                        <button className='main-btn' onClick={() => addInputField('image')}>Attach Image</button>
+                        <button className='main-btn' onClick={() => addInputField('pdf')}>Attach PDF</button>
+                        <button className='main-btn' onClick={() => addInputField('link')}>Add Link</button>
+                        <button className='main-btn' onClick={() => addInputField('code-block')}>Code Block</button>
+                        {/* Add more buttons for other types as needed */}
+                    </div>
+                </div>
+
+                <div className='confirm-content'>
+                    <button className='remove-btn'>Discard</button>
+                    <button className='continue-btn'>Continue</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
 
 export default Project;
