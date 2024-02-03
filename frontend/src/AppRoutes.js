fb76d0cfee6df190584a1628de63e17ec9be8009
utkarsh.ver <utkarshv2005@gmail.com>
2024-02-04 02:42:53 +0530

    route

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -6,6 +6,7 @@ import Project from './components/pages/Project/Project';
 import Infobar from './components/pages/Home/Infobar';
 import About from './components/pages/AboutUs/About';
 import Profile from './components/pages/Profile/Profile';
+import Home from './components/pages/Home/Home';
 
 const AppRoutes = () => {
     return (
@@ -14,7 +15,7 @@ const AppRoutes = () => {
                 <Route path="/" element={<About />} />
                 <Route path="/Login" element={<Login />} />
                 <Route path="/Profile" element={<Profile />} />
-
+                <Route path="/Home" element={<Home/>}></Route>
                 <Route path="/EditProfile" element={<EditProfile />} />
                 <Route path="/Project" element={<Project />} />
                 <Route path="/Infobar" element={<Infobar />} />
--- frontend/src/components/pages/Profile/Profile.js
+++ frontend/src/components/pages/Profile/Profile.js
@@ -1,8 +1,11 @@
+
 import React from "react";
 import {useState,useEffect} from "react";
 import "./Profile.css";
 import Sidebar from "./Sidebar";
 import axios from "axios";
+import Navbar from "../../header/Navbar";
+
 import arrow from '../../assets/side-arrow.png'
 import { constants } from "constants-browserify";
 
@@ -18,37 +21,26 @@ const Profile = () => {
   const [active, setActive] = React.useState('ongoing');
   return (
     <div className="profile">
+    
+      <Navbar/>
+        <Sidebar />
 
-      <div className="div-3">
-        <div className="rectangle-3" />
-        <div className="text-wrapper-11">Project Overview</div>
-        <div className="frame-29">
-          <div className="text-wrapper-12">{profile[0].name}</div>
+        <div className="profile_nav">
+          <div className="profile_pjt_view">Project Overview</div>
+          <div className="profile_ongoing_btn">Ongoing</div>
+          <div className="profile_complete_btn">Complete</div>
+          <div className="profile-box">
+            <div className="profile-box-1"></div>
+          </div>
         </div>
-        <div className="frame-30">
-          <div className="text-wrapper-12">{profile[1].name}</div>
-        </div>
-        <div className="overlap-group">
-          <div className="rectangle-4" />
-          <div className="rectangle-5" />
-        </div>
-        <div className="rectangle-6" />
-        <div className="rectangle-7" />
-        <Sidebar
-          className="profile-nav-instance"
-          frameClassName="profile-nav-2"
-          frameClassName1="profile-nav-7"
-          frameClassNameOverride="profile-nav-4"
-          frameFrameClassName="profile-nav-3"
-          iconParkOutline="/img/icon-park-outline-degree-hat-1.svg"
-          imgClassName="profile-nav-5"
-          imgClassNameOverride="profile-nav-6"
-          mingcuteSchoolLine="/img/mingcute-school-line.svg"
-          property1="section-1"
-          vectorClassName="design-component-instance-node"
-        />
+
       </div>
-    </div>
+      
+      
+    
+      
+
+    
   );
 };
 
