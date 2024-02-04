30e6f630b89d675bcedbe877e3754e6ab8d281dc
ShashwatSharma15 <shashwatsharma.mohali@gmail.com>
2024-02-05 00:46:48 +0530

    project page

--- frontend/src/components/pages/Project/Project.js
+++ frontend/src/components/pages/Project/Project.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../header/Navbar';
 import "./Project.css"
-
+import Review from './Review';
 
 const Project = () => {
   const [inputFields, setInputFields] = useState([]);
@@ -24,17 +24,66 @@ const Project = () => {
     localStorage.setItem('inputFields', JSON.stringify(inputFields));
   }, [inputFields]);
 
+//   function inputType(field){
+//     if(field.type == "heading" || "subheading"){
+//         <input type="text" className={`project-${field.type}`} placeholder={`Enter ${field.type} here`} />
+//     }else if(field.typr == "description"){
+//         <input type="textarea" className={`project-${field.type}`} placeholder={`Enter ${field.type} here`} />
+//     }
+//   }
+
+
+
+const inputType = (field) => {
+    const adjustHeight = (e) => {
+      e.target.style.height = 'inherit'; // Reset height to recalculate
+      e.target.style.height = `${e.target.scrollHeight}px`; // Set new height
+    };
+  
+    if (field.type === 'heading' || field.type === 'subheading' || field.type=== 'link') {
+      return (
+        <input
+          type="text"
+          className={`project-${field.type}`}
+          placeholder={`Enter ${field.type} here`}
+        />
+      );
+    } else if (field.type === 'description'|| field.type === 'code-block') {
+      return (
+        <textarea
+          className={`project-${field.type}`}
+          placeholder={`Enter ${field.type} here`}
+          onInput={adjustHeight}
+        />
+      );
+    } else if (field.type == 'image'){
+        return (
+            <input type='file' className={`project-${field.type}`}
+            placeholder={`Add Image`}
+            />
+        );
+    
+    } else if (field.type == 'pdf'){
+        return (
+            <input type='file' className={`project-${field.type}`}
+            placeholder={`Add pdf`}
+            />
+        );
+    }
+  };
+  
   return (
     <div>
-        
+        <Navbar/>
         
         <div className='project-main'>
-        <Navbar/>
+
             <div className='content-shown'>
                 <form>
                     {inputFields.map((field, index) => (
                     <div key={index}>
-                        <input type="text" className={`project-${field.type}`} placeholder={`Enter ${field.type} here`} />
+                        {/* <input type="text" className={`project-${field.type}`} placeholder={`Enter ${field.type} here`} /> */}
+                        {inputType(field)}
                     </div>
                     ))}
                 </form>
@@ -47,7 +96,6 @@ const Project = () => {
                         <button className='main-btn' onClick={() => addInputField('heading')}>Add Heading</button>
                         <button className='main-btn' onClick={() => addInputField('subheading')}>Add Subheading</button>
                         <button className='main-btn' onClick={() => addInputField('description')}>Add Description</button>
-                        <button className='main-btn' onClick={() => addInputField('caption')}>Add Caption</button>
                         <button className='main-btn' onClick={() => addInputField('image')}>Attach Image</button>
                         <button className='main-btn' onClick={() => addInputField('pdf')}>Attach PDF</button>
                         <button className='main-btn' onClick={() => addInputField('link')}>Add Link</button>
@@ -62,8 +110,12 @@ const Project = () => {
                 </div>
             </div>
         </div>
+
+        <Review
+            
+        />
     </div>
   );
 };
 
-export default Project;
+ export default Project;
