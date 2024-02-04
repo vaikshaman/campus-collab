e262e042fa166930592e4b1095ed246734893058
jatinweb2004 <varmajatin2004@gmail.com>
2024-02-04 19:58:40 +0530

    all done

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -7,15 +7,20 @@ import Infobar from './components/pages/Home/Infobar';
 import About from './components/pages/AboutUs/About';
 import Profile from './components/pages/Profile/Profile';
 import AskQuery from './components/pages/Query/AskQuery';
+import Courses from './components/pages/Courses/Courses';
+import Home from './components/pages/Home/Home';
 
 const AppRoutes = () => {
     return (
         <Router> 
             <Routes>
                 <Route path="/" element={<About />} />
+                <Route path="/Home" element={<Home/>}/>
                 <Route path="/Login" element={<Login />} />
                 <Route path="/Profile" element={<Profile />} />
+                <Route path="/About" element={<About/>}/>
                 <Route path="/Askquery" element={<AskQuery/>}/>
+                <Route path='/Courses' element={<Courses/>}/>
                 <Route path="/EditProfile" element={<EditProfile />} />
                 <Route path="/Project" element={<Project />} />
                 <Route path="/Infobar" element={<Infobar />} />
