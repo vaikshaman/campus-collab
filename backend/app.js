ce7682e3da9d713fcf33a28ba34dcb7d6f0eb1e6
vaikshaman <133702921+vaikshaman@users.noreply.github.com>
2024-04-01 15:23:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,13 @@ connectDB();
 
 // Create an instance of Express
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: {"https://deploy-mern-1whq.vercel.app"},
+        methods: ["POST", "GET"],
+        credentials: true,
+    }
+));
 
 // Set view engine
 app.set("view engine", "es");
