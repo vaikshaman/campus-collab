3ec3e3d9c4e8d18eb09e8f0bb7af5bbf00fa7e1f
vaikshaman <133702921+vaikshaman@users.noreply.github.com>
2024-06-12 15:22:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 // import mongoose from "mongoose";
 // import router from './routes/router.js';
 // import path from 'path';
-const uploadsDirectory = path.join(process.cwd(), 'uploads');
+// const uploadsDirectory = path.join(process.cwd(), 'uploads');
 
 
 // Configure dotenv
@@ -26,7 +26,7 @@ app.set("view engine", "es");
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 //for accessing uploads folder
-app.use('/uploads', express.static(uploadsDirectory));
+// app.use('/uploads', express.static(uploadsDirectory));
 
 // Middleware for parsing incoming request bodies
 app.use(express.urlencoded({ extended: true }));
